bb13688ecbb89e7ab5843f4dca72ad77ffc28f97
Pavel <pasharadio@yahoo.com>
2017-05-06 00:31:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,28 +25,28 @@ $(".collapse").click(function() {
 
 // media query event handler
 if (matchMedia) {
-  var mq = window.matchMedia("(max-width: 768px)");
-  mq.addListener(WidthChange);
-  WidthChange(mq);
+    var mq = window.matchMedia("(max-width: 768px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
 }
 
 // media query change
 function WidthChange(mq) {
-  if (mq.matches) {
-    // window width is at least 500px
-  } else {
-    $(window).on("scroll", function() {
-    var menu = $(".header-fixed"),
-        begin = $("header").height();
-    if ($(this).scrollTop() > begin && !menu.hasClass("scrolled"))
-        if (screen.width > '300' && $(window).width() > '300') {
-            menu.addClass("scrolled").fadeIn(300);
-        }
-    if ($(this).scrollTop() <= begin && menu.hasClass("scrolled")) {
-        menu.removeClass("scrolled").fadeOut(0);
+    if (mq.matches) {
+        // window width is at least 500px
+    } else {
+        $(window).on("scroll", function() {
+            var menu = $(".header-fixed"),
+                begin = $("header").height();
+            if ($(this).scrollTop() > begin && !menu.hasClass("scrolled"))
+                if (screen.width > '300' && $(window).width() > '300') {
+                    menu.addClass("scrolled").fadeIn(300);
+                }
+            if ($(this).scrollTop() <= begin && menu.hasClass("scrolled")) {
+                menu.removeClass("scrolled").fadeOut(0);
+            }
+        });
     }
-});
-  }
 
 }
 
@@ -58,3 +58,50 @@ function WidthChange(mq) {
 //   var scrolled = $(window).scrollTop();
 //   $('.prlx').css('margin-bottom',+(scrolled*0.03)+'px');
 // }
+
+jQuery(document).ready(function() {
+    jQuery('.home-slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1200,
+        arrows: false,
+    });
+});
+
+jQuery(document).ready(function() {
+    jQuery('.home-partial-product-slider').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1200,
+        speed:400,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        //asNavFor: '.slider-for',
+        //centerMode: true,
+        centerPadding: '10%',
+        //focusOnSelect: true,
+        infite:true,
+        draggable:true,
+       
+    });
+});
+jQuery(document).ready(function() {
+    jQuery('.home-partial-product-slider>.treeslider').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1200,
+        speed:400,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        //asNavFor: '.slider-for',
+        //centerMode: true,
+        //centerPadding: '20%',
+        //focusOnSelect: true,
+        infite:true,
+        draggable:true,
+       
+    });
+});
+
