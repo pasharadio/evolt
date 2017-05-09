59f5d597c9b697898f7f0b4b3ee8a1b9cc7ab0bc
Pavel <pasharadio@yahoo.com>
2017-05-09 15:25:05 +0300

    .

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 $(".collapsecircle").click(function() {
     $(this).parent().children(".cat-catalog").toggle();
 
-
 });
 $(".collapsecircle").click(function() {
     $(this).find("i").toggleClass('fa-times fa-plus');
@@ -15,6 +14,28 @@ $(".collapse").click(function() {
 $(".collapse").click(function() {
     $(this).find("i").toggleClass('fa-caret-up fa-caret-down');
 });
+<!--collapsible tabs mob-->
+$(".collapse_tabs").click(function() {
+    $(this).parent().children(".tabs-content-inner").toggle('slow'); 
+});
+$(".collapse_tabs").click(function() {
+    $(this).find("i").toggleClass('fa-chevron-down fa-chevron-up');
+});
+//accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
 
 
 
@@ -49,16 +70,16 @@ jQuery(document).ready(function() {
         arrows: false,
         autoplay: true,
         autoplaySpeed: 1200,
-        speed:400,
+        speed: 400,
         slidesToShow: 3,
         slidesToScroll: 1,
         //asNavFor: '.slider-for',
         //centerMode: true,
         centerPadding: '10%',
         //focusOnSelect: true,
-        infite:true,
-        draggable:true,
-       
+        infite: true,
+        draggable: true,
+
     });
 });
 jQuery(document).ready(function() {
@@ -67,16 +88,16 @@ jQuery(document).ready(function() {
         arrows: false,
         autoplay: true,
         autoplaySpeed: 1200,
-        speed:400,
+        speed: 400,
         slidesToShow: 1,
         slidesToScroll: 1,
         //asNavFor: '.slider-for',
         //centerMode: true,
         //centerPadding: '20%',
         //focusOnSelect: true,
-        infite:true,
-        draggable:true,
-       
+        infite: true,
+        draggable: true,
+
     });
 });
 jQuery(document).ready(function() {
@@ -85,16 +106,89 @@ jQuery(document).ready(function() {
         arrows: false,
         autoplay: true,
         autoplaySpeed: 1200,
-        speed:400,
+        speed: 400,
         slidesToShow: 3,
         slidesToScroll: 3,
         //asNavFor: '.slider-for',
         centerMode: true,
         //centerPadding: '20%',
         //focusOnSelect: true,
-        infite:true,
+        infite: true,
         //draggable:true,
-       
+
     });
 });
-
+jQuery(document).ready(function() {
+    jQuery('.product-mainimage-slider').slick({
+        dots: false,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 1200,
+        speed: 400,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infite: true,
+        prevArrow: '<i class="fa fa-chevron-left slick-prev" aria-hidden="true"></i>',
+        nextArrow: '<i class="fa fa-chevron-right slick-next" aria-hidden="true"></i>',
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                //slidesToShow: 3,
+                //slidesToScroll: 3,
+                //infinite: true,
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                //slidesToShow: 3,
+                //slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 320,
+            settings: {
+                //slidesToShow: 2,
+                //slidesToScroll: 1.
+                dots:true,
+                arrows:false
+            }
+        }]
+    });
+});
+jQuery(document).ready(function() {
+    jQuery('.product-page-related_product-slider').slick({
+        dots: false,
+        arrows: true,
+        //autoplay: true,
+        autoplaySpeed: 1200,
+        speed: 400,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '',
+        nextArrow: '<i class="fa fa-chevron-right slick-next" aria-hidden="true"></i>',
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: true,
+                centerPadding: '24%'
+            }
+        }]
+    });
+});
